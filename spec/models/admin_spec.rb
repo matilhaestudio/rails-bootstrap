f0ca8bae0e5dde7ea444aae2e56cf68e1d341ecd
Marcelo Barreto <marcelobarretojunior@gmail.com>
2017-02-18 02:06:54 -0200

    Admin model specs

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    context 'presence_of' do
+      it { expect(subject).to validate_presence_of(:email) }
+    end
+
+    context 'uniqueness_of' do
+      it { expect(subject).to validate_uniqueness_of(:email).case_insensitive }
+    end
+  end
+
+  context 'when Agency is valid' do
+    subject(:admin) { build(:admin) }
+    it { expect(admin).to be_valid }
+  end
+
+  context 'when Agency is not valid' do
+    subject(:admin) { build(:admin, email: 'admin-invalid-email') }
+    it { expect(admin).not_to be_valid }
+  end
 end
